871bc7d53de2f66d0a6a8cd900d253b36a4c7cc5
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2024-04-11 08:46:21 +0300

    Added .prettier and .eslintrc

--- src/App.js
+++ src/App.js
@@ -1,54 +1,55 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import styles from './App.module.scss'
-import {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import styles from './App.module.scss';
+import { useState } from 'react';
 
 library.add(fas);
 
 function App() {
-    const [value, setValue] = useState('home')
-// Добавим начальную картинку
+  const [value, setValue] = useState('home');
+  // Добавим начальную картинку
 
-    let iconsNameArray, randomIndex, randomIcon, result;
+  let iconsNameArray, randomIndex, randomIcon, result;
 
-    const nameCalculate = () => {
-        iconsNameArray = Object.keys(fas).map((iconName) => {
-            return iconName
-        })
-        randomIndex = Math.floor(Math.random() * iconsNameArray.length);
-        randomIcon = iconsNameArray[randomIndex];
+  const nameCalculate = () => {
+    iconsNameArray = Object.keys(fas).map((iconName) => {
+      return iconName;
+    });
+    randomIndex = Math.floor(Math.random() * iconsNameArray.length);
+    randomIcon = iconsNameArray[randomIndex];
 
-// на 24 сбрасываем значение, чтобы пушить в чистый массив, а не удлинять название иконки
-        result = []
-// в цикле меняем имя из библиотеки с верблюжьего на дефисный (кебабный?)
-        for (let i = 0; i < randomIcon.length; i++) {
-            const element = randomIcon[i];
-            if (element !== element.toUpperCase()) {
-                result.push(element)
-            } else {
-                result.push('-');
-                result.push(element.toLowerCase())
-            }
-        }
-
-        setValue(result.join(''))
-    }
-// задержка в 3 секунды
-    const delayHandler = () => {
-        setTimeout(() => {
-            nameCalculate()
-        }, 3000);
+    // на 23 сбрасываем значение, чтобы пушить в чистый массив, а не удлинять название иконки
+    result = [];
+    // в цикле меняем имя рандомной иконки из библиотеки с верблюжьего на дефисный (кебабный?)
+    for (let i = 0; i < randomIcon.length; i++) {
+      const element = randomIcon[i];
+      if (element !== element.toUpperCase()) {
+        result.push(element);
+      } else {
+        result.push('-');
+        result.push(element.toLowerCase());
+      }
     }
 
-    return (
-        <div className={styles.container}>
-            <button onClick={delayHandler} className={styles.button}>
-                click me!
-            </button>
-            <FontAwesomeIcon icon={`${value}`} className={styles.icon}/>
-        </div>
-    );
+    setValue(result.join(''));
+  };
+
+  // задержка в 3 секунды
+  const delayHandler = () => {
+    setTimeout(() => {
+      nameCalculate();
+    }, 3000);
+  };
+
+  return (
+    <div className={styles.container}>
+      <button onClick={delayHandler} className={styles.button}>
+        click me!
+      </button>
+      <FontAwesomeIcon icon={`${value}`} className={styles.icon} />
+    </div>
+  );
 }
 
 export default App;
